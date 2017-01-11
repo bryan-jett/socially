de34be6bb1eeb1aee4d55d8bd44cc367eb56bfa3
Bryan Jett <bryan.jett@experian.com>
2017-01-10 21:34:07 -0800

    Step 2 & 3 of tutorial

--- client/imports/app/app.component.js
+++ client/imports/app/app.component.js
@@ -12,6 +12,20 @@ var core_1 = require('@angular/core');
 var app_component_html_1 = require('./app.component.html');
 var AppComponent = (function () {
     function AppComponent() {
+        this.parties = [
+            { 'name': 'Dubstep-Free Zone',
+                'description': 'Can we please just for an evening not listen to dubstep.',
+                'location': 'Palo Alto'
+            },
+            { 'name': 'All dubstep all the time',
+                'description': 'Get it on!',
+                'location': 'Palo Alto'
+            },
+            { 'name': 'Savage lounging',
+                'description': 'Leisure suit required. And only fiercest manners.',
+                'location': 'San Francisco'
+            }
+        ];
     }
     AppComponent = __decorate([
         core_1.Component({
